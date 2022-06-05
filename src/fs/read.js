f2c181f2f8056cfa775cbf4be47fa38188dea4a4
TSepron <proffizitdollara@gmail.com>
2022-06-05 18:51:47 +0300

    Implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import { fileURLToPath } from 'url'
+import { readFile } from 'fs/promises'
+
 export const read = async () => {
-    // Write your code here 
-};
+    const thisScriptPath = fileURLToPath(import.meta.url)
+
+    try {
+        const contents = await readFile(`${thisScriptPath}/../files/fileToRead_.txt`)
+        console.log(contents.toString())
+    } catch (err) {
+        if (['ENOENT'].includes(err.code))
+            throw new Error('FS operation failed')
+
+        throw err
+    }
+}
+
+read()
