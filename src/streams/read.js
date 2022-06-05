93b01524eadd909557424da6f39afa2bff26d12d
TSepron <proffizitdollara@gmail.com>
2022-06-05 22:24:47 +0300

    Implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,13 @@
+import { createReadStream } from "fs"
+import { stdout } from "process"
+import { fileURLToPath } from "url"
+
 export const read = async () => {
-    // Write your code here 
-};
+    const thisScriptPath = fileURLToPath(import.meta.url)
+
+    const input = createReadStream(`${thisScriptPath}/../files/fileToRead.txt`)
+    input
+        .pipe(stdout)
+}
+
+read()
