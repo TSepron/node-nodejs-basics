d7dc30a904195c49afa6bf7bb8313bd427d1af3b
TSepron <proffizitdollara@gmail.com>
2022-06-05 23:14:42 +0300

    Implement transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,23 @@
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+
 export const transform = async () => {
-    // Write your code here 
-};
+    const upperCaseTr = new Transform({
+        transform(chunk, encoding, cb) {
+            const result = chunk
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+
+            this.push(`${result}\n`)
+            cb();
+        }
+    });
+
+    stdin
+        .pipe(upperCaseTr)
+        .pipe(stdout)
+}
+
+transform()
