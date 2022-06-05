9787c7055765ed2fa01366a07fd79385fdae2a4d
TSepron <proffizitdollara@gmail.com>
2022-06-05 19:46:18 +0300

    Implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,28 @@
+/* 
+    In powershell set env by typing in terminal:
+        $env:RSS_something='something special'
+    or
+        $env:other_value='other value'
+    then just launch this script
+
+    In bash set env by typing in terminal:
+        export RSS_something='something special'
+    or
+        export other_value='other value'
+    then just launch this script
+*/
+
 export const parseEnv = () => {
-    // Write your code here 
-};
+    const PREFIX = 'RSS_'
+
+    const ourEnvNames = Object.keys(process.env)
+        .filter(envName => envName.startsWith(PREFIX))
+
+    console.log(
+        ourEnvNames
+            .map(envName => `${envName}=${process.env[envName]}`)
+            .join('; ')
+    )
+}
+
+parseEnv()
