ccb0f4487eceaa0cf2b190228303ae8283762d84
TSepron <proffizitdollara@gmail.com>
2022-06-05 22:38:53 +0300

    Implement write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+// node src/streams/write.js
+// typing something
+// Ctrl + C
+// then look at fileToWrite.txt
+
+import { createWriteStream } from "fs"
+import { stdin } from "process"
+import { fileURLToPath } from "url"
+
 export const write = async () => {
-    // Write your code here 
+    const thisScriptPath = fileURLToPath(import.meta.url)
+
+    const output = createWriteStream(`${thisScriptPath}/../files/fileToWrite.txt`)
+    stdin.pipe(output)
 };
+
+write()
