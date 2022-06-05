516960c2f1e52f6e079942bb73248998fab0793d
TSepron <proffizitdollara@gmail.com>
2022-06-05 18:36:21 +0300

    Implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,19 @@
+import { rm } from 'fs/promises'
+import { fileURLToPath } from 'url'
+
 export const remove = async () => {
-    // Write your code here 
-};
+    const thisScriptPath = fileURLToPath(import.meta.url)
+
+    try {
+        await rm(
+            `${thisScriptPath}/../files/fileToRemove.txt`
+        )
+    } catch (err) {
+        if (['ENOENT'].includes(err.code))
+            throw new Error('FS operation failed')
+
+        throw err
+    }
+}
+
+remove()
