ecd9f259c6df0435b996e2bb42a6d7cfec080f57
TSepron <proffizitdollara@gmail.com>
2022-06-09 04:46:13 +0300

    Implement decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,23 @@
+import { createReadStream, createWriteStream } from 'fs'
+import { pipeline } from 'stream/promises'
+import { fileURLToPath } from 'url'
+import { createUnzip } from 'zlib'
+
+
 export const decompress = async () => {
-    // Write your code here 
-};
+    const thisScriptPath = fileURLToPath(import.meta.url)
+
+    const unzip = createUnzip()
+
+    const source = createReadStream(
+        `${thisScriptPath}/../files/archive.gz`
+    )
+
+    const destination = createWriteStream(
+        `${thisScriptPath}/../files/fileToCompress.txt`
+    )
+
+    await pipeline(source, unzip, destination)
+}
+
+decompress()
