b6209e0ebc0c308b53f4a0f9dd229b292ce6efa0
TSepron <proffizitdollara@gmail.com>
2022-06-09 06:01:16 +0300

    Implement worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,37 @@
-// n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { fileURLToPath } from "url"
+import { isMainThread, parentPort, Worker, workerData } from "worker_threads"
+
+export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2)
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
-};
+    const thisScriptPath = fileURLToPath(import.meta.url)
+
+    if (isMainThread) {
+        const worker = new Worker(thisScriptPath, {
+            workerData: {
+                n: 33
+            },
+            resourceLimits: {
+                maxOldGenerationSizeMb: 4
+            }
+        })
+            .on('message', msg => console.log(msg))
+            .on('error', err => {
+                throw err
+            })
+            .on('exit', code => {
+                if (code !== 0)
+                    throw new Error(`Worker stopped with exit code ${code}`)
+
+                console.log(`worker with thread id '${worker.threadId}' finish his work)`)
+                clearTimeout(workerTerminateTimerId)
+            })
+
+        // if worker won't finish it job for 10 sec, terminate it
+        const workerTerminateTimerId = setTimeout(() => worker.terminate(), 10000)
+    } else {
+        parentPort.postMessage(nthFibonacci(workerData.n))
+    }
+}
+
+sendResult()
